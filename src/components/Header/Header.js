7a4600b26f9a09f269c2f1505d07ee177ea6cf06
Mr-Robot-101 <git.sujon@gmail.com>
2022-10-12 13:59:40 +0600

    Tostify Double Showing Problem Fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,12 @@ import logo from '../../Images/logo-1.png'
 import './Header.css'
 import avatar from '../../Images/me.jpg'
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 
 
 const Header = () => {
 const [notifi, setNotifi] =useState(2)
+
     const notifiHandler =() => {
         setNotifi(0)
         toast.success(`New Update Release 🔥🔥🔥`);
@@ -111,8 +112,8 @@ const [notifi, setNotifi] =useState(2)
                                         d="M224 512c35.32 0 63.97-28.65 63.97-64H160.03c0 35.35 28.65 64 63.97 64zm215.39-149.71c-19.32-20.76-55.47-51.99-55.47-154.29 0-77.7-54.48-139.9-127.94-155.16V32c0-17.67-14.32-32-31.98-32s-31.98 14.33-31.98 32v20.84C118.56 68.1 64.08 130.3 64.08 208c0 102.3-36.15 133.53-55.47 154.29-6 6.45-8.66 14.16-8.61 21.71.11 16.4 12.98 32 32.1 32h383.8c19.12 0 32-15.6 32.1-32 .05-7.55-2.61-15.27-8.61-21.71z">
                                     </path>
                                 </svg>
-                                <span onClick={()=>notifiHandler()} className="text-white bg-red-700 absolute rounded-full  ml-2 py-0 px-1 mb-4">{notifi}
-                                <ToastContainer autoClose={1500} position="top-right" toastStyle={{ backgroundColor: "rgba(255, 249, 166, 0.9)", color: "#000", fontSize: "20px" }} />
+                                <span onClick={notifiHandler} className="text-white bg-red-700 absolute rounded-full  ml-2 py-0 px-1 mb-4">{notifi}
+                               
                                 </span>
                             </Link>
 
